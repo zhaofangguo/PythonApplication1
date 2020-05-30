1c55c37ad344e862a6a6ddd7d223f2034fff5478
zhaofangguo <2055466817@qq.com>
2020-05-30 15:27:07 +0800

    一次commit尝试

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -3,7 +3,7 @@ import class1
 #zhaofangguo.print_name()
 #zhaofangguo.print_age()
 
-class_name=input("name:")
+class_name=input("名字:")
 class_age=input("age:")
 class_sex_def=input("sex:")
 zhaofangguo=male(class_name,class_age,class_sex_def)
